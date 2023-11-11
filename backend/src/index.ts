c094c5497b145599468f9b277652a6bab1fe5eb8
Franklin <franklin1014.op@gmail.com>
2023-11-11 20:07:33 +0100

    testing api function

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,3 +4,12 @@ import cors from 'cors';
 const app = express();
 
 app.use(express.json());
+app.use(cors());
+
+app.get('/api/recipes/search', async (req, res) => {
+  res.json({ message: 'sucess!!' });
+});
+
+app.listen(5000, () => {
+  console.log('Server running on Port 5000');
+});
