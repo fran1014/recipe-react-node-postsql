c9efa2e6f376cdbc11a6afe2cee2f426600dbded
Franklin <franklin1014.op@gmail.com>
2023-11-11 20:50:30 +0100

    backend connection running

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -10,6 +10,6 @@ app.get('/api/recipes/search', async (req, res) => {
   res.json({ message: 'sucess!!' });
 });
 
-app.listen(5000, () => {
-  console.log('Server running on Port 5000');
+app.listen(5100, () => {
+  console.log('Server running on Port 5100');
 });
