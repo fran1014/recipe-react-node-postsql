5f68a141bfeddd507ae1019e8989e7677c69f593
Franklin <franklin1014.op@gmail.com>
2023-11-12 17:39:31 +0100

    chage port

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -10,6 +10,6 @@ app.get('/api/recipes/search', async (req, res) => {
   res.json({ message: 'sucess!!' });
 });
 
-app.listen(5100, () => {
-  console.log('Server running on Port 5100');
+app.listen(5110, () => {
+  console.log('Server running on Port 5110');
 });
