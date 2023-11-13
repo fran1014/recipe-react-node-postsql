1f98e5d3da66f0c08f4a41cbbc0e9cc080fdc921
Franklin <franklin1014.op@gmail.com>
2023-11-13 19:16:21 +0100

    added try and catch response block

--- backend/src/recipe-api.ts
+++ backend/src/recipe-api.ts
@@ -1,11 +1,15 @@
 const apiKey = process.env.API_KEY;
 
-const searchRecipes = (searchTerm: string, page: number) => {
+const searchRecipes = async (searchTerm: string, page: number) => {
   if (!apiKey) {
     throw new Error('Api Key not found');
   }
 
   const url = new URL('https://api.spoonacular.com/recipes/complexSearch');
+
+  try {
+    const searchResponse = await fetch(url);
+  } catch (error) {}
 
   const queryParams = {
     apiKey,
