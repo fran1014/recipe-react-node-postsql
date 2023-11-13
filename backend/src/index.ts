5b537546856fd9759184026002deeaf4d43369a7
Franklin <franklin1014.op@gmail.com>
2023-11-14 00:08:48 +0100

    return reasults on json

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -11,6 +11,8 @@ app.get('/api/recipes/search', async (req, res) => {
   const searchTerm = req.query.searchTerm as string;
   const page = parseInt(req.query.page as string);
   const results = RecipeAPI.searchRecipes(searchTerm, page);
+
+  return res.json(results);
 });
 
 app.listen(4000, () => {
